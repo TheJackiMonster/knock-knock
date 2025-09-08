27d3e77e72d89b97678b81441fd09f6d4a1b2c30
Jacki <jacki@thejackimonster.de>
2025-09-08 23:39:35 +0200

    Fix install script
    Signed-off-by: Jacki <jacki@thejackimonster.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,8 +8,9 @@ cp LICENSE /usr/local/share/knock-knock/LICENSE
 cp door.py /usr/local/share/knock-knock/door.py
 cp door.sh /usr/local/share/knock-knock/door.sh
 cp door.ui /usr/local/share/knock-knock/door.ui
-cp door.ui /usr/local/share/knock-knock/door_row.ui
-cp door.ui /usr/local/share/knock-knock/new_door_dialog.ui
+cp door_row.ui /usr/local/share/knock-knock/door_row.ui
+cp new_door_dialog.ui /usr/local/share/knock-knock/new_door_dialog.ui
+cp edit_door_dialog.ui /usr/local/share/knock-knock/edit_door_dialog.ui
 
 mkdir -p /usr/local/bin
 cp bin/door /usr/local/bin/knock-knock
