3cf2a2097469d25e89b6e9455501ac357a782dca
Jacki <jacki@thejackimonster.de>
2025-09-09 03:44:41 +0200

    Add custom icon
    Signed-off-by: Jacki <jacki@thejackimonster.de>

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 cd $(dirname $0)
 cd ..
 
+APPLICATION_ID="de.thejackimonster.KnockKnock"
+
 mkdir -p /usr/local/share/knock-knock
 cp AUTHORS /usr/local/share/knock-knock/AUTHORS
 cp LICENSE /usr/local/share/knock-knock/LICENSE
@@ -12,8 +14,14 @@ cp door_row.ui /usr/local/share/knock-knock/door_row.ui
 cp new_door_dialog.ui /usr/local/share/knock-knock/new_door_dialog.ui
 cp edit_door_dialog.ui /usr/local/share/knock-knock/edit_door_dialog.ui
 
+cp icon/64x64.png /usr/local/share/icons/hicolor/64x64/apps/$APPLICATION_ID.png
+cp icon/128x128.png /usr/local/share/icons/hicolor/128x128/apps/$APPLICATION_ID.png
+cp icon/256x256.png /usr/local/share/icons/hicolor/256x256/apps/$APPLICATION_ID.png
+cp icon/512x512.png /usr/local/share/icons/hicolor/512x512/apps/$APPLICATION_ID.png
+cp knock-knock.svg /usr/local/share/icons/hicolor/scalable/apps/$APPLICATION_ID.svg
+
 mkdir -p /usr/local/bin
 cp bin/knock-knock /usr/local/bin/knock-knock
 
 mkdir -p /usr/local/share/applications
-cp knock-knock.desktop /usr/local/share/applications/knock-knock.desktop
+cp knock-knock.desktop /usr/local/share/applications/$APPLICATION_ID.desktop
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,6 +2,14 @@
 cd $(dirname $0)
 cd ..
 
-rm -f /usr/local/share/applications/knock-knock.desktop
+APPLICATION_ID="de.thejackimonster.KnockKnock"
+
+rm -r /usr/local/share/icons/hicolor/64x64/apps/$APPLICATION_ID.png
+rm -r /usr/local/share/icons/hicolor/128x128/apps/$APPLICATION_ID.png
+rm -r /usr/local/share/icons/hicolor/256x256/apps/$APPLICATION_ID.png
+rm -r /usr/local/share/icons/hicolor/512x512/apps/$APPLICATION_ID.png
+rm -r /usr/local/share/icons/hicolor/scalable/apps/$APPLICATION_ID.svg
+
+rm -f /usr/local/share/applications/$APPLICATION_ID.desktop
 rm -rf /usr/local/share/knock-knock/
 rm -f /usr/local/bin/knock-knock
